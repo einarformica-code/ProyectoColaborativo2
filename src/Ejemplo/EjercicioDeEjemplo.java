e0181a87171f5c93e3fd27567269c91c1041d7cf
GonzaloPG <gonzalo.prada@iesnervion.es>
2026-01-19 12:23:54 +0100

    añadire esto a mi clase

---
+++ src/Ejemplo/EjercicioDeEjemplo.java
@@ -0,0 +1,37 @@
+package Ejemplo;
+
+public class EjercicioDeEjemplo {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Hola mundo, he realizado mi propia rama");
+		
+		
+		int contador=0;
+		
+		int i=6;
+		
+		int j=28;
+		
+		int w=2;
+		
+		int x=9;
+		
+	do {		
+			
+		i+=w*x+j/i;
+		
+		contador++;
+		
+		i++;
+		
+	}while(contador>=5);
+		
+	
+		System.out.println(i*2/4);
+		
+		
+	}
+
+}
