acbbd5bd0074e1ba7db3bb29bd9e053e34fe2be6
einarformica-code <einar.formica@iesnervion.es>
2026-01-20 14:36:38 +0100

    Cambios Einar

--- src/PaqueteEinar/ClaseEinar.java
+++ src/PaqueteEinar/ClaseEinar.java
@@ -1,9 +1,12 @@
 package PaqueteEinar;
 
+import java.util.Scanner;
+
 public class ClaseEinar {
 	
 	String prueba="Esto es un mensaje de prueba";
-	
+	Scanner teclado = new Scanner(System.in);
+	int numeroHoras4;
 	int numeroHoras=3;
 
 	}
